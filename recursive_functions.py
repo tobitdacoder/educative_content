7cf1070367318e3629db91ad14b22ea6ba1ff15b
tobit bushenyula <mrbushenyula@gmail.com>
2023-02-15 10:37:01 -0800

    factorial_new
    just a slight update of the factorial function, here we are now asking for the user input

--- recursive_functions.py
+++ recursive_functions.py
@@ -1,7 +1,14 @@
+def main():
+   num=int(input("give me a number and i will give you it's factorial: "))
+   print(fact(num))
+
+#here we have created a function called fact which will calculate the factorial of every number given
+
 def fact(n):
    if n==1:
       return n
    else:
       return n*fact(n-1)
-print(fact(5))
+
 #this is to learn recursion and how it works
+main()
