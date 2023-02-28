ad3d5e58aa3fec5f2d117b7f2b16904fc2390497
tobit bushenyula <mrbushenyula@gmail.com>
2023-02-28 10:21:00 -0800

    Update simple_function_creation.py
    simple initial explanation

--- simple_function_creation.py
+++ simple_function_creation.py
@@ -6,3 +6,5 @@ def myName(name):
     print(f"Hello, {name}")
     
 main()
+#here we have created a simple function that will return a sentense containing a specific string entered with the created sub-function
+
