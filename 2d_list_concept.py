07587d7a468a02067807ddb61233428084642b5f
tobit bushenyula <mrbushenyula@gmail.com>
2023-02-27 16:59:04 -0800

    2d_list
    this is a 2d_list which can be accessed using the index number of the different numbers using the row index and the column index

---
+++ 2d_list_concept.py
@@ -0,0 +1,24 @@
+number_grid=[
+   [0,1,-2,3,4,-5,-6,7,-8,9],
+   [-10,11,-12,13,14,15,16,-17,18,-19],
+   [73,8,-91,4,50,6,700,-8,-63,44],
+   [0]
+] #here we are introducing the concept of accessing a list by using the index number of the row and the column
+# elements,here we are using what is callrd a 2d list (which means that we have two dimensions used which are the rows
+# columns)
+
+#this is called a 2D list, it is a list which elements are also lists, and to access the content, we use this function
+# comand below 👇
+
+print(number_grid[1][2])
+print(number_grid[1][5])
+print(number_grid[1][2])
+print(number_grid[2][9])
+print(number_grid[1][6])
+print(number_grid[3][0])
+print(number_grid[2][5])
+print(number_grid[1][2])
+
+#here the [1][2]:
+# the [1] is the index of the row and the [2] is the index of the colomn... both indicates the element 6.
+#
