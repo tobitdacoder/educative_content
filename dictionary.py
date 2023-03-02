7b487f60ca9157a359de7b483e30f4541e0c72d5
tobit bushenyula <mrbushenyula@gmail.com>
2023-03-01 23:10:59 -0800

    MonthsDictionary.py
    this is a simple dictionary we create. her we created a dictionary containing the months of the year and to print each month name, we are using its key
    ... further explanation will be done with time

---
+++ dictionary.py
@@ -0,0 +1,37 @@
+#DICTIONARIES!!!!!!!!!
+
+monthDictionary={
+   "jan": "january",
+   "feb": "february",
+   "mar": 3 ,# this is just to try is the key can print a number or an expression result 😊 
+   "apr": "april",
+   "ma": "may",
+   "jun": "june",
+   "jul": "jully",
+   "aug": "august",
+   "sep": "september",
+   "oct": "october",
+   "nov": "november",
+   "dec": "december",
+#here in this dictionary : "jan" is a key and "january is it's value"
+#these keys can also be numbers like 1: "january" or 2:"february"
+   
+}
+
+#this is the first way to access the dictionary content
+print(monthDictionary["mar"])
+print(monthDictionary["feb"])
+
+#the second way is: 
+print(monthDictionary.get("nov","not a valid key"))
+#the cool thing with this is that we can set a default value that 
+#  we can use if the key is not found: eg. print(monthDictionary.get("luv","not a valid key"))
+# "luv" which is not in the key set... 
+# here the default value will be "not a valid key" 
+
+
+def change(num):
+    return -1*num
+    
+result=change(4)
+print(result)
