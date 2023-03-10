ee93fce9271636e6865e8bd69e0cb22405767260
tobit bushenyula <mrbushenyula@gmail.com>
2023-03-09 17:24:54 -0800

    Fundamentals.py
    these are some of the very fundamentals that we are going to use in our everyday life as python devellopers. the list will be updated as we are learning day by day

---
+++ fundamentalTerms.py
@@ -0,0 +1,102 @@
+#firstname="john"
+#print("my name is " + firstname) 
+
+name="I am A Student"
+
+print(name.lower())
+print(name.upper())
+
+phrase="this is me"
+print(phrase.index("s"))
+
+jina="Miye njo tobit"
+print(jina.upper())
+
+
+#this is to give us the index of the chosen letter
+#by passing "me" to that print statement, it will show us where that word begins
+#in the string
+
+print(phrase.replace("me", "you"))
+
+#this replace function replaces the first world by the second one
+
+my_num=-564
+print(abs(my_num))
+
+#this abs function is used to find the absolute value of the value u gave
+
+print(pow(5,4))
+
+#this means "5 power 4" or "5^4"
+
+print(max(4,5,6,-7))
+
+#this function returns the largest of the number passed to it
+from math import *
+
+# this allows us to import external functions in our code from differentexternal libraries
+
+print(min(4,5,6,-7))
+
+#this prints the minimum of them
+
+print(round(4565.45))
+
+#arrondir par exces ou par defaut 
+
+
+
+ans=print(abs(-54.654))
+
+print(round(57465.4454))
+
+num="it is just a trial"
+
+print("yes i am \n" * 19)
+
+
+
+from math import *
+#to access external math functions
+
+print(floor(4.9))
+print(ceil(3.2))
+
+#floor will print the lowest value of the decimal value given
+
+#the ceil function is the oposite of the previous function
+
+print(sqrt (25))
+
+#this prints the square root of the value given
+
+
+age=input("how old are you?: ")
+
+name=input("what is your name: ")
+
+print(f" {name} you are {age}!")
+
+#getting input from a user
+
+#num1=input("enter first number: ")
+#num2=input("enter second number: ")
+
+
+
+#print("1. addition\n")
+
+#print("2. substraction\n")
+
+#print("3. multiplication\n")
+
+#print("4. division\n")
+
+#opp=input ("choose an operator: ")
+
+#if opp== 1:
+#   print("answer is:" + (num1+num2))
+#else if opp== 2:
+#  print("answer is:" + (num1-num2))
+  
