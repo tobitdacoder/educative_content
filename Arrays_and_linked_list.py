2f5124ea2843dbdb6d4ce7f376831ecbd7f9c8c9
tobit bushenyula <mrbushenyula@gmail.com>
2023-03-10 22:57:57 -0800

    Arrays_and_linked_list.py
    this file will contains notes about arrays and linked lists theory and introduction

---
+++ Arrays_and_linked_list.py
@@ -0,0 +1,9 @@
+import numpy as np
+lst = list(range(100))
+print(lst)
+
+#in this code we are importing the "numpy" library and we use one of its functions wich is list()
+
+a = ['foo', 'bar', 'baz', 'qux', 'quux', 'corge']
+
+print(a[-5:-3])
