91943add3ec3e46958c3d12e987229a6fe4aef85
roby bush <mrbushenyula@gmail.com>
2023-05-22 12:28:16 -0700

    Create more_trick_and_knowledge_2.py
    here are some new ways to use strings

---
+++ more_trick_and_knowledge_2.py
@@ -0,0 +1,15 @@
+
+""" THIS IS THE CONTINUATION OF THE FIRST FILE ABOUT MORE TRICKS WE ARE LEARNING VERYDAY IN PYTHON"""
+
+########################################################################
+
+myString='tobit comment ca va ?'
+#print(myString[:4]) #here the emplty space before the colomn will be filled by the default value which is 0.
+#print(myString[:]) #here we will get the entire string 
+
+#this is cool but it still very manual and can be challenging when strings are bigger
+
+#print(myString[ : :1]) #here we want to add the gap (here is the indexes that will be jumped before to print the next index)
+
+#print(myString[ : :-1]) #here we are going backward
+print("welcome back",myString.split()[0]) #this function turns the string into a list of individual strings separated by the space char ... the index there now allows us to use different parts of that whole string
