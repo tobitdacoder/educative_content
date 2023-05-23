ebaf0ffd83462b8ba443e8ab090bcbc563cc718a
roby bush <mrbushenyula@gmail.com>
2023-05-23 11:55:37 -0700

    Update more_trick_and_knowledge_2.py
    here we have updated our small program and we've also added some more relevant information

--- more_trick_and_knowledge_2.py
+++ more_trick_and_knowledge_2.py
@@ -18,6 +18,8 @@ print("welcome back",myString.split()[0]) #this function turns the string into a
 
 #this small program helps us to capture any vowel found in the string or the set of strings entered by the user (we give them colors just to differenciate them)
 
+""" this can be used in kids game if we want them to identify the vowels in a sentense or in a paragraph"""
+
 vowels=["a","e","i","o","u"]
 
 striing=input("a string please: ")
@@ -26,7 +28,21 @@ for letter in striing:
   if letter.lower() in vowels: 
     print("\033[32m",end='') #these end="" are helping us to eliminate the space in between the letters after the color text is writen
   print(letter,end='') #this end here helps us to print an horizontal sentense by eliminating the default \n 
-  print("\033[0m",end='') 
+  print("\033[0m",end='')  #Hehe we comw back to white
 
   #the if condition here will help us capture any vowel that is found in our string provided by the user. (this is sooo helpful)
+  
+""" 
+THESE ARE THE COLOURS codes to be used in such games 
+
+\033[30m  #BLACK OR GRAY
+\033[31m  #RED
+\033[32m  #GREEN
+\033[33m  #YELLOW
+\033[34m  #SKY BLUE
+\033[35m  #PINK
+\033[00m  #WHITE
+
+"""
+
     
