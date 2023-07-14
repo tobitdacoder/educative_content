8c3f0cd328d7c6c2d16206b484f0d40aa7e579d1
roby bush <mrbushenyula@gmail.com>
2023-07-14 09:52:55 -0700

    Create More_tricks_and_knowledge_3.py
    Today is the DAY 65 of the 100 days of code. We have a game creation challenge today and we will be using Object Oriented Programming for that, Just for learning purpose and for Fun too.

---
+++ More_tricks_and_knowledge_3.py
@@ -0,0 +1,12 @@
+""" THIS IS THE CONTINUATION OF THE SECOND FILE ABOUT MORE TRICKS WE ARE LEARNING EVERYDAY IN PYTHON.
+REMEMBER THAT, THIS IS A COMPILLATION OF MULTIPLE PROGRAMS. IF YOU WANT TO TRY OUT A SPECIFIC ONE, COPY IT AND PASTE IT IN YOUR ONLINE PYTHON COMPILER OF YOUR CHOICE AND TRY IT OUT !!!!"""
+
+################################################################ 
+
+# DAY 65 CHALLENGE: create a game using Object Oriented Programming
+
+class MyCharacter:
+  # 1. we innitiate our variables to be used in our objects
+  Name=None
+  Health=None
+  MagicPoints=None
