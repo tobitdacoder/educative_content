7bbb836a00d9e8226573ae3c2ec2828f8d8ad825
roby bush <mrbushenyula@gmail.com>
2023-07-28 14:37:36 -0700

    Update More_tricks_and_knowledge_3.py
    DAY 76: we are introduced to flask, the tool that will help us create our web server. This web server will help us make our website dynamic (can change, based on the person or even based on what the person what it to show ). Now this is the introduction to flask and how to start using it.

--- More_tricks_and_knowledge_3.py
+++ More_tricks_and_knowledge_3.py
@@ -504,7 +504,27 @@ else:
       time.sleep(1)
       os.system("clear")
       continue
-  
+
+#################################################################### 
+
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return 'Hello from Flask!'
+
+@app.route('/home') # Creates the path to the home page
+def home(): # Subroutine to create the home page
+  page = """"""
+  # Three quotes - I'm going to write or paste my HTML code here. The HTML gets assigned to the page variable
+  return page # returns the contents of the page variable
+
+
+app.run(host='0.0.0.0', port=81)
+
   
   
   
