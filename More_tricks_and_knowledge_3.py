857df2fbdd8cac2d3ab46ac1d50e98386fdceda7
tobit bushenyula <mrbushenyula@gmail.com>
2023-10-05 16:10:25 +0100

    Update More_tricks_and_knowledge_3.py
    this is a program that basically get the numerator and the denominator from the user and find their Gcd before to return them into a simplified version.

--- More_tricks_and_knowledge_3.py
+++ More_tricks_and_knowledge_3.py
@@ -306,6 +306,46 @@ while True:
 
 #####################################################
 
+from math import gcd
+
+class Fraction:
+    
+    def __init__(self,Numerator,Denominator):
+        
+        self.Numerator=Numerator
+        self.Denominator=Denominator
+        
+        
+    def GetNumbers(self):
+        
+        num1=int(input("num1: "))
+        while True:
+            num2=int(input("num2: "))
+            if num2==0:
+                print("not valid")
+                continue
+            break
+        self.Numerator=num1
+        self.Denominator=num2
+        
+        
+    def Gcd(self):
+        
+        common=gcd(self.Numerator,self.Denominator)
+        newNumer=self.Numerator/common
+        newDeno=self.Denominator/common 
+        fraction=str(int(newNumer))+"/"+str(int(newDeno))
+        return fraction
+
+if __name__=="__main__":
+    
+    Objectone=Fraction(0,0)
+    Objectone.GetNumbers()
+    print(Objectone.Gcd())
+
+
+#####################################################
+
 from replit import db as database
 
 # this small program helps us to verify if the database is empty by checking the number of keys inside the database. 
