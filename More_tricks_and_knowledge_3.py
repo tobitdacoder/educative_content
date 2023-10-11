aa4222b47eb350db34a3ad54ff1772bb85f3133f
tobit bushenyula <mrbushenyula@gmail.com>
2023-10-11 09:16:29 +0100

    Update More_tricks_and_knowledge_3.py
    this is how that code works.

--- More_tricks_and_knowledge_3.py
+++ More_tricks_and_knowledge_3.py
@@ -389,6 +389,8 @@ print(arrr)
 
 #####################################################
 
+# I just wrote a simple python program to iterate a given list and count the occurrence of
+#each element and created a dictionary to show the count of each element.
 
 count_list=[11,45,8,11,23,45,23,45,89]
 
