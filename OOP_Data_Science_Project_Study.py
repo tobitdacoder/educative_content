3683ed1c1b53a49ee1ec8b9ee2561c8f07accd6b
tobit bushenyula <mrbushenyula@gmail.com>
2023-10-31 19:28:03 +0000

    Update OOP_Data_Science_Project_Study.py
    Here, we have defined the Titanic class and an init method that takes the path to the Titanic dataset as input.

--- OOP_Data_Science_Project_Study.py
+++ OOP_Data_Science_Project_Study.py
@@ -1,4 +1,13 @@
 
+
+"""
+Here, we have defined the Titanic class and an init method that takes the path to the Titanic dataset as input.
+ When we try to create an object of the Titanic class, the following steps will happen:
+
+- the class definition will be used to create an instance of the Titanic class
+- the init method will read the Titanic dataset into a pandas dataframe and store it as an instance variable self.data.
+
+"""
 import pandas as pd 
 
 class Titanic:
