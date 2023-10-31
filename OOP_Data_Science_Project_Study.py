b4894e78dce1c2a6d22e1716889f2f23193ff125
tobit bushenyula <mrbushenyula@gmail.com>
2023-10-31 19:47:43 +0000

    Update OOP_Data_Science_Project_Study.py
    here we are now creating a method that will clean and pre-process the data, so that we can always call this function later when we want to clean data.

--- OOP_Data_Science_Project_Study.py
+++ OOP_Data_Science_Project_Study.py
@@ -14,4 +14,18 @@ class Titanic:
 
     def __init__(self,data_path):
         self.data=pd.read_csv(data_path)
+
+    def preprocess_data(self):
+        """Preprocess the data to handle missing values and convert categorical variables to numerical"""
+        self.data.fillna(self.data.mean(), inplace=True) # fill missing values with mean
+        self.data = pd.get_dummies(self.data, columns=['Embarked', 'Sex']) # convert categorical to numerical
+
+        """
+        With this class method defined, all we have to do in future to preprocess our data is to simply call 
+        the function called preprocess_data and pass the object name as the argument. The entire logic and code
+          of cleaning and preparing the data is now “abstracted” in the form of a simple to use method.
+          
+        """
+
+
         
